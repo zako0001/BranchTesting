7f12a00ef7644ae412bd53050bdc211b6ad947f6
zako0001 <zako0001@stud.kea.dk>
2024-04-23 15:02:04 +0200

    testet makesound af i main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+
+        Animal cat = new Cat();
+        Animal dog = new Dog();
+
+        cat.makeSound();
+        dog.makeSound();
     }
 }
